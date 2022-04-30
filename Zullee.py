ab29a97f3e07285801e0062159328e0fd338711d
maggiexio <53416822+maggiexio@users.noreply.github.com>
2022-04-30 16:17:49 -0400

    Update Zullee.py

--- Zullee.py
+++ Zullee.py
@@ -54,7 +54,7 @@ with col11:
         """)
       allHours=df_ori['Hour'].drop_duplicates()
       default_hour=['All']
-      default_hour.extend(state_1)
+      default_hour.extend(allHours)
       hour_choice=st.multiselect("", default_hour)
       if ('All' in hour_choice):
         df_ori_1=df_ori
@@ -90,36 +90,9 @@ with col11:
   st.markdown(f'<h3 style="text-aligh: center;color: green;">{title_ch1}</h3>',unsafe_allow_html=True)
   title_ch2='****2D interactive plots********'
   st.markdown(f'<h4 style="text-aligh: center;color: green;">{title_ch2}</h4>',unsafe_allow_html=True)
-  with st.expander("Histogram:   distributions of sum score for male/female, under different test-taking mode: take the test at home or not "):    
-    fig_hist1=px.histogram(df_1, x='sum_score', color='gender', facet_col='home_computer', marginal='box')
-    st.plotly_chart(fig_hist1,  use_container_width=True, height=600)
-  with st.expander("Bar charts:    sum score distribution for each age group"): 
-    sorted_df = df_1.sort_values(by='age')
-    sorted_df = sorted_df.reset_index(drop=True)
-    opac = st.text_input('Opacity(0-1)', '0.8')
-    fig_bar1=px.bar(sorted_df, y='sum_score', color='age_group', facet_row='age_group', opacity=float(opac), facet_row_spacing=0.01)
-    st.plotly_chart(fig_bar1, use_container_width=True, height=400)
-  with st.expander("Animation:    display the sum score pattern across states and the relationship with age"):  
-    fig_ani1=px.bar(df_1, x='age_group', animation_frame='state_abbr', color='gender')
-    fig_ani1.update_layout(transition = {'duration': 30000})
-    st.plotly_chart(fig_ani1,  use_container_width=True, height=600)
-    fig_ani2=px.scatter(df_1, y='sum_score', x='age', animation_frame='state_abbr', color='gender', size='rt_scale', size_max=60)
-    fig_ani2.update_layout(transition = {'duration': 30000})
-    st.plotly_chart(fig_ani2,  use_container_width=True, height=600)   
-  with st.expander("Pie Charts:    check sum score distribution under country and state"):    
-    fig_3=px.sunburst(df_1, color='sum_score',  path=['country_abbr','state_abbr'])
-    st.plotly_chart(fig_3,   use_container_width=True, height=600)
-  with st.expander("Tree Map:    check total response time distribution under country and state"):    
-    fig_tree=px.treemap(df_1, color='rt_total',  path=['country_abbr','state_abbr'])
-    st.plotly_chart(fig_tree, use_container_width=True, height=600)    
-  with st.expander("choropleth map:    check score distribution from a choropleth map"):
-    mean_df = df_1.groupby("country_abbr").mean()
-    mean_df.reset_index(inplace=True)
-    mean_df = mean_df.rename(columns = {'index':'country_abbr'})
-    fig_4=px.choropleth(mean_df, color='sum_score',  locations='country_abbr', locationmode='ISO-3')
-    st.plotly_chart(fig_4,  use_container_width=True, height=600)
+
   title_ch3='****3D interactive plots********'
   st.markdown(f'<h4 style="text-aligh: center;color: green;">{title_ch3}</h4>',unsafe_allow_html=True)
-  with st.expander("Check the relationship between total score, age, and test-taking mode (home taking or not) in an interactive 3D way"): 
-    fig_scatter1=px.scatter_3d(df_1, y='sum_score', x='age', z='home_computer', color='gender', size='rt_scale', size_max=50)
+  with st.expander("Check the relationship between Month, hour and net sales in an interactive 3D way"): 
+    fig_scatter1=px.scatter_3d(df_1, y='Net_Sales', x='Hour', z='Month', color='Month', size='Net_Sales', size_max=50)
     st.plotly_chart(fig_scatter1,  use_container_width=True, height=3000)
